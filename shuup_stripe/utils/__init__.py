9bda51a9c0aa38ba43c9d5d0a9397e57b4a8f6c6
Janne Tammi <janne@shuup.com>
2018-05-10 13:47:34 -0700

    Move utils to package

---
+++ shuup_stripe/utils/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup Stripe Addon.
+#
+# Copyright (c) 2012-2018, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the AGPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
+from .general import get_amount_info
+
+__all__ = [
+    "get_amount_info",
+]
--- shuup_stripe/utils/general.py
+++ shuup_stripe/utils/general.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # This file is part of Shuup Stripe Addon.
 #
-# Copyright (c) 2012-2016, Shoop Commerce Ltd. All rights reserved.
+# Copyright (c) 2012-2018, Shoop Commerce Ltd. All rights reserved.
 #
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
-
 ZERO_DECIMAL_CURRENCIES = (
     # https://support.stripe.com/questions/which-zero-decimal-currencies-does-stripe-support
     "BIF", "CLP", "DJF", "GNF", "JPY", "KMF", "KRW", "MGA",
