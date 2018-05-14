fd305c69a584adfa2f813a7eb24cbb8e74d66849
Janne Tammi <janne@shuup.com>
2018-05-14 09:13:56 -0700

    Move AppConfig to separate file

--- shuup_stripe/__init__.py
+++ shuup_stripe/__init__.py
@@ -6,36 +6,10 @@
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
 import pkg_resources
-from shuup.apps import AppConfig
 
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except pkg_resources.DistributionNotFound:
     __version__ = None
 
-
-class ShuupStripeAppConfig(AppConfig):
-    name = "shuup_stripe"
-    verbose_name = "Shuup Stripe Checkout integration"
-    label = "shuup_stripe"
-
-    provides = {
-        "front_service_checkout_phase_provider": [
-            "shuup_stripe.checkout_phase:StripeCheckoutPhaseProvider",
-        ],
-        "service_provider_admin_form": [
-            "shuup_stripe.admin_forms:StripeCheckoutAdminForm",
-        ],
-        "payment_processor_wizard_form_def": [
-            "shuup_stripe.admin_forms:StripeCheckoutWizardFormDef",
-        ],
-        "front_urls_pre": [
-            "shuup_stripe.urls:urlpatterns"
-        ],
-        "admin_module": [
-            "shuup_stripe.admin_module:StripeAdminModule"
-        ]
-    }
-
-
-default_app_config = __name__ + ".ShuupStripeAppConfig"
+default_app_config = __name__ + ".apps.ShuupStripeAppConfig"
